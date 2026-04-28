596650b28f0a5f592c099d54e158156306133d64
ourbanor <omar.urbano-rendon@wsu.edu>
2026-04-27 22:26:13 -0700

    Added unique users stat, couple of styling changes

--- frontend/src/pages/AdminDash.jsx
+++ frontend/src/pages/AdminDash.jsx
@@ -12,6 +12,7 @@ export default function AdminDash(){
     const [recentOrders, setRecentOrders] = useState([]);
     const [sales, setSales] = useState({});
     const [selectedSale, setSelectedSale] = useState(0.0);
+    const [uniqueUsers, setUniqueUsers] = useState(0);
 
 
     const fetchTotalUsers = async () => {
@@ -143,6 +144,25 @@ export default function AdminDash(){
         }
     }
 
+    const fetchUniqueUsers = async () => {
+        const response = await fetch(`http://localhost:8000/api/orders/uniqueusersweekly/`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+
+        if(response.ok) {
+            const data = await response.json();
+            console.log("This is unique users: ", data);
+            setUniqueUsers(data);
+        }
+        else
+        {
+            console.error("Failed to fetch unique users: ", response.status);
+        }
+    }
+
 
     useEffect(() => {
         fetchTotalUsers();
@@ -152,6 +172,7 @@ export default function AdminDash(){
         fetchAverageSale();
         fetchRecentOrders();
         fetchSales();
+        fetchUniqueUsers();
 
     }, []);
 
@@ -161,28 +182,33 @@ export default function AdminDash(){
         <div className="container shadow-xl pb-4">
 
             <div className="flex items-center justify-between my-4 mx-4"> 
-                <a href="#" className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium">
+                <a href="#" className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium transform h-32 transition duration-500 hover:scale-110">
                     <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Total Users</h5>
+                    <hr className="w-full my-2"/>
                     <p className="text-body">{totalUsers}</p>
                 </a>
 
-                <a href="#" className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium">
+                <a href="#" className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium transform h-32 transition duration-500 hover:scale-110">
                     <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Total Products</h5>
+                    <hr className="w-full my-2"/>
                     <p className="text-body">{totalProducts}</p>
                 </a>
 
-                <a href="#" className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium">
+                <a href="#" className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium transform h-32 transition duration-500 hover:scale-110">
                     <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Total Categories</h5>
+                    <hr className="w-full my-2"/>
                     <p className="text-body">{totalCategories}</p>
                 </a>
 
-                <a href="#" className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium">
+                <a href="#" className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium transform h-32 transition duration-500 hover:scale-110">
                     <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Average Sale</h5>
+                    <hr className="w-full my-2"/>
                     <p className="text-body">${averageSale}</p>
                 </a>
 
-                <a href="#" className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium">
+                <a href="#" className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium transform h-32 transition duration-500 hover:scale-110">
                     <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Total Orders</h5>
+                    <hr className="w-full my-2"/>
                     <p className="text-body">{totalOrders}</p>
                 </a>
             </div>
@@ -198,27 +224,27 @@ export default function AdminDash(){
             <div className="flex items-center justify-center gap-10 my-10">
                 {/*  */}
 
-                <div className="max-w-sm w-full bg-neutral-primary-soft border border-default rounded-base shadow-xs p-4 md:p-6">
-                    <div className="flex justify-between items-start">
+                <div className="max-w-sm w-full h-40 bg-neutral-primary-soft border border-default rounded-base shadow-xs p-4 md:p-6 transform h-48 transition duration-500 hover:scale-110">
+                    <div className="mb-4">
+                        <label className="text-2xl font-bold text-heading my-2">Unique users this week</label>
+                        <hr className="w-full"/>
+                    </div>
+                    <div className="flex justify-center">
                         <div>
-                            <h5 className="text-2xl font-semibold text-heading">32.4k</h5>
-                            <p className="text-body">Users this week</p>
-                        </div>
-                        <div className="flex items-center px-2.5 py-0.5 font-medium text-fg-success text-center">
-                            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v13m0-13 4 4m-4-4-4 4"/></svg>
-                            12%
+                            <label className="text-2xl font-semibold text-heading">{uniqueUsers}</label>
                         </div>
                     </div>
                     
                 </div>
 
-                {/*  */}
-
-
-                <div className="max-w-sm w-full bg-neutral-primary-soft border border-default rounded-base shadow-xs p-4 md:p-6">
+                <div className="max-w-sm w-full h-40 bg-neutral-primary-soft border border-default rounded-base shadow-xs p-4 md:p-6 transform h-48 transition duration-500 hover:scale-110">
+                    <div className="mb-4">
+                        <label className="text-2xl font-bold text-heading my-2">Sales</label>
+                        <hr className="w-full"/>
+                    </div>
                     <div className="flex justify-center">
                         <div>
-                        <h5 className="text-2xl font-bold text-heading">${selectedSale}</h5>
+                            <label className="text-2xl font-bold text-heading">${selectedSale}</label>
                         </div>
                     </div>
                     <div className="my-2">
@@ -239,10 +265,6 @@ export default function AdminDash(){
 
             </div>
             
-
-            
-            
-
         </div>
 
 );
